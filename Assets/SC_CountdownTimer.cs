f7663238733b028ac921961059204d840fb58611
marionjorda <marionjorda@next-u.fr>
2024-05-30 20:14:33 +0200

    modif trash + timer

---
+++ Assets/SC_CountdownTimer.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SC_CountdownTimer : MonoBehaviour
+{
+
+    public enum CountdownFormatting { DaysHoursMinutesSeconds, HoursMinutesSeconds, MinutesSeconds, Seconds };
+    public CountdownFormatting countdownFormatting = CountdownFormatting.DaysHoursMinutesSeconds;
+    public bool showMilliseconds = true;
+    public double countdownTime = 600;
+
+    public Text countdownText;
+    double countdownInternal;
+    bool countdownOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdownText = GetComponent<Text>();
+        countdownInternal = countdownTime;
+    }
+
+    void Update() {
+        if(countdownInternal > 0) {
+            countdownInternal -= Time.deltaTime;
+            if(countdownInternal < 0) {
+                countdownInternal = 0;
+            }
+            countdownText.text = FormatTime(countdownInternal, countdownFormatting, showMilliseconds);
+
+            if (countdownInternal <= 15)
+            {
+                countdownText.color = Color.Lerp(Color.white, Color.red, Mathf.Sin(Time.time * 5));
+            }
+        }
+        else {
+            if(!countdownOver) {
+                countdownOver = true;
+                Debug.Log("Countdown Over!");
+                // countdownText.text = "00:00:00   Countdown Over!";
+            }
+        }
+    }
+
+string FormatTime(double time, CountdownFormatting formatting, bool includeMilliseconds) {
+    string timeText = "";
+
+    int intTime = (int)time;
+    int days = intTime / 86400;
+    int hoursTotal = intTime / 3600;
+    int hoursFormatted = hoursTotal % 24;
+    int minutesTotal = intTime / 60;
+    int minutesFormatted = minutesTotal % 60;
+    int secondsTotal = intTime;
+    int secondsFormatted = intTime % 60;
+    int milliseconds = (int)(time * 100);
+    milliseconds = milliseconds % 100;
+
+     if (includeMilliseconds)
+        {
+            if (formatting == CountdownFormatting.DaysHoursMinutesSeconds)
+            {
+                timeText = string.Format("{0:00}:{1:00}:{2:00}:{3:00}:{4:00}", days, hoursFormatted, minutesFormatted, secondsFormatted, milliseconds);
+            }
+            else if (formatting == CountdownFormatting.HoursMinutesSeconds)
+            {
+                timeText = string.Format("{0:00}:{1:00}:{2:00}:{3:00}", hoursTotal, minutesFormatted, secondsFormatted, milliseconds);
+            }
+            else if (formatting == CountdownFormatting.MinutesSeconds)
+            {
+                timeText = string.Format("{0:00}:{1:00}:{2:00}", minutesTotal, secondsFormatted, milliseconds);
+            }
+            else if (formatting == CountdownFormatting.Seconds)
+            {
+                timeText = string.Format("{0:00}:{1:00}", secondsTotal, milliseconds);
+            }
+        }
+        else
+        {
+            if (formatting == CountdownFormatting.DaysHoursMinutesSeconds)
+            {
+                timeText = string.Format("{0:00}:{1:00}:{2:00}:{3:00}", days, hoursFormatted, minutesFormatted, secondsFormatted);
+            }
+            else if (formatting == CountdownFormatting.HoursMinutesSeconds)
+            {
+                timeText = string.Format("{0:00}:{1:00}:{2:00}", hoursTotal, minutesFormatted, secondsFormatted);
+            }
+            else if (formatting == CountdownFormatting.MinutesSeconds)
+            {
+                timeText = string.Format("{0:00}:{1:00}", minutesTotal, secondsFormatted);
+            }
+            else if (formatting == CountdownFormatting.Seconds)
+            {
+                timeText = string.Format("{0:00}", secondsTotal);
+            }
+        }
+
+        return timeText;
+    }
+}
